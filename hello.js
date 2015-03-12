30b178dcf67090daa424dfca0ca7395aa39edddd
Andy Pemberton <pembertona@gmail.com>
2015-03-12 16:49:04 -0400

    close server

--- hello.js
+++ hello.js
@@ -10,5 +10,15 @@ var server = http.createServer(function (request, response) {
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
 
+// Count down from 10 seconds
+(function countDown (counter) {
+  console.log(counter);
+  if (counter > 0)
+    return setTimeout(countDown, 1000, counter - 1);
+
+  // Close the server
+  server.close(function () { console.log('Server closed!'); });
+})(4);
+
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
